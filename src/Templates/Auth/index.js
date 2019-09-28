7e10c93cae95eeac1b6f1ff552b7ed5813a08d36
kimbogyu <bg0820@naver.com>
2019-09-28 18:51:27 +0900

    기본적인 색상 코드 및 배경색 적용

---
+++ src/Templates/Auth/index.js
@@ -0,0 +1,13 @@
+import React, {PureComponent} from 'react';
+
+class MainTemplate extends PureComponent {
+	render() {
+		return (
+			<div className="">
+				
+			</div>
+		);
+	}
+}
+
+export default MainTemplate;
