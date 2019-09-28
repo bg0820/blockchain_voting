28384bf2b2c54463f6b35abd8882c6ffea39059a
kimbogyu <bg0820@naver.com>
2019-09-28 18:33:59 +0900

    plus/minus Button

--- src/App.js
+++ src/App.js
@@ -3,14 +3,41 @@ import React, {PureComponent} from 'react';
 import Button from '@components/Button';
 
 class App extends PureComponent {
+	constructor() {
+		super();
+		
+		this.state = {
+			val: 0
+		}
+
+		console.log("1");
+	}
+
+	componentDidMount() {
+		console.log("2");
+	}
+
+	plus = () => {
+		this.setState({
+			val: this.state.val + 1
+		})
+	}
+
+	minus = () => {
+		this.setState({
+			val: this.state.val - 1
+		})
+	}
+
+
 
 	render() {
 		return (
 			<div className="">
-				<Button value="로그인"></Button>
-				<Button value="회원가입"></Button>
-				<Button></Button>
-				<Button></Button>
+				<Button value="+" handleClick={this.plus}></Button>
+				<Button value="-" handleClick={this.minus}></Button>
+				
+				<p>{this.state.val}</p>
 			</div>
 		)
 	}
--- src/Components/Button/index.js
+++ src/Components/Button/index.js
@@ -3,10 +3,19 @@ import React, {PureComponent} from 'react';
 import './index.scss';
 
 class Button extends PureComponent {
+
+	componentDidMount() {
+		console.log("3");
+	}
+
+	handleClick = () => {
+		this.props.handleClick();
+	}
+
 	render() {
 		return (
 			<div className="Button">
-				<input type="button" value={this.props.value}></input>
+				<input type="button" value={this.props.value} onClick={this.handleClick}></input>
 			</div>
 		)
 	}
