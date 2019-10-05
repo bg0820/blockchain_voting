a969f365442151e9d2bf7f9fbbd13ee1d4669923
kimbogyu <bg0820@naver.com>
2019-10-05 16:14:27 +0900

    투표 화면 상단 구현

--- src/Pages/Voting/index.js
+++ src/Pages/Voting/index.js
@@ -6,7 +6,6 @@ import './index.scss'
 
 @inject('page')
 class Voting extends PureComponent {
-
 	clickCallback = (e) => {
 		const {page} = this.props;
 		page.setPage('voting');
@@ -15,7 +14,14 @@ class Voting extends PureComponent {
 	render() {
 		return (
 			<div className="Voting">
-				<div className="NavBar">← 성공회대학교 제 33대 총학생회 선거</div>
+				<div className="NavBar">
+					<span className="I_LEFT_ARROW"></span>
+					<div className="voteTitle">
+						<p>
+							성공회대학교 제 33대 총학생회 선거
+						</p>
+					</div>
+				</div>
 			</div>
 		);
 	}
