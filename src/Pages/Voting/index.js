a27142c2a75de71b28a6dd37c8a8594c7946b916
심민정 <minjony1014@gmail.com>
2019-10-05 16:23:15 +0900

    Auto stash before merge of "master" and "origin/master"

--- src/Pages/Voting/index.js
+++ src/Pages/Voting/index.js
@@ -2,13 +2,12 @@ import React, {PureComponent} from 'react';
 import { observer, inject } from 'mobx-react';
 
 import './index.scss'
+import Button from '../../Components/Button';
 
 
 @inject('page')
 class Voting extends PureComponent {
 	clickCallback = (e) => {
-		const {page} = this.props;
-		page.setPage('voting');
 	}
 
 	render() {
@@ -18,10 +17,20 @@ class Voting extends PureComponent {
 					<span className="I_LEFT_ARROW"></span>
 					<div className="voteTitle">
 						<p>
-							성공회대학교 제 33대 총학생회 선거
 						</p>
+							성공회대학교 제 33대 총학생회 선거
 					</div>
 				</div>
+                    <div className="Candidate">
+                    <div className="CandidateNum">기호 1번 (늘품) </div>
+                    <div className="CandidateCard">
+                        <div className="ButtonWrap">
+                            style='reverse' clickCallback={this.clickCallback}></Button>
+                            <Button value='경력 및 공략' 
+                            <img className="VoteMark" src="/image/vote_mark.png" />
+                            <div className="Touch">터치</div>
+                        </div>
+                    </div>
 			</div>
 		);
 	}
